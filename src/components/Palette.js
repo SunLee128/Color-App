6ac5a3bfd0deab8146b4a3077b0190bde9b4c16a
sunlee <sunlee128@gmail.com>
2020-02-19 11:22:35 +1100

    Palette Styles refactored

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -3,6 +3,18 @@ import ColorBox from 'components/ColorBox';
 import './Palette.css';
 import Navbar from 'components/Navbar';
 import PaletteFooter from './PaletteFooter';
+import { withStyles } from "@material-ui/styles";
+
+const styles = {
+  Palette: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column"
+  },
+  colors: {
+    height: "90%"
+  }
+};
 
 class Palette extends Component {
   constructor (props) {
@@ -22,6 +34,7 @@ class Palette extends Component {
 
   render () {
     const { colors, paletteName, emoji, id } = this.props.palette;
+    const { classes } = this.props;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
       <ColorBox
@@ -34,17 +47,17 @@ class Palette extends Component {
     ));
 
     return (
-      <div className='Palette'>
+      <div className={classes.Palette}>
         <Navbar
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
           showingAllColors
         />
-        <div className='Palette-colors'> {colorBoxes} </div>
+        <div className={classes.colors}>{colorBoxes}</div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
 }
-export default Palette;
+export default withStyles(styles)(Palette);
