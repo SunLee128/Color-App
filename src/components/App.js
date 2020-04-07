11bc7acc93f38f7ab7f3abeacf57b1f1cb24d4e1
sunlee <sunlee128@gmail.com>
2020-04-08 08:45:06 +1000

    test commented out

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Palette from 'components/Palette';
 import PaletteList from 'components/PaletteList';
 import SingleColorPalette from './SingleColorPalette';
@@ -15,25 +15,27 @@ class App extends Component {
 
   render () {
     return (
-      <Switch>
-        <Route
-          exact
-          path="/palette/:paletteId/:colorId"
-          render={(routeProps) => (
-            <SingleColorPalette
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
-            />
-          )}
-        />
-        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
-        <Route
-          exact
-          path="/palette/:id"
-          render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}
-        />
-        <Route exact path="/palette/:paletteId/:colorId" render={() => <h1>SINGLE COLOR PAGE!</h1>} />
-      </Switch>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/palette/:paletteId/:colorId"
+            render={(routeProps) => (
+              <SingleColorPalette
+                colorId={routeProps.match.params.colorId}
+                palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+              />
+            )}
+          />
+          <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}
+          />
+          <Route exact path="/palette/:paletteId/:colorId" render={() => <h1>SINGLE COLOR PAGE!</h1>} />
+        </Switch>
+      </BrowserRouter>
 
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
