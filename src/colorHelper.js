a85ab46fb15e9857ea8a245e9e76b5b2eb8a2b18
sunlee <sunlee128@gmail.com>
2020-04-30 08:51:58 +1000

    Saving New Palettes

--- src/colorHelper.js
+++ src/colorHelper.js
@@ -1,28 +1,36 @@
 import chroma from 'chroma-js';
-const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+const levels = [
+  50,
+  100,
+  200,
+  300,
+  400,
+  500,
+  600,
+  700,
+  800,
+  900
+];
 
 function generatePalette (starterPalette) {
-  const newPalette = {
-    paletteName: starterPalette.paletteName,
-    id: starterPalette.id,
-    emoji: starterPalette.emoji,
-    colors: {}
+  let newPalette = {
+    paletteName : starterPalette.paletteName,
+    id          : starterPalette.id,
+    emoji       : starterPalette.emoji,
+    colors      : {}
   };
-  for (const level of levels) {
+  for (let level of levels) {
     newPalette.colors[level] = [];
   }
-  for (const color of starterPalette.colors) {
-    const scale = getScale(color.color, 10).reverse();
-    for (const i in scale) {
+  for (let color of starterPalette.colors) {
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
       newPalette.colors[levels[i]].push({
-        name: `${color.name} ${levels[i]}`,
-        id: color.name.toLowerCase().replace(/ /g, '-'),
-        hex: scale[i],
-        rgb: chroma(scale[i]).css(),
-        rgba: chroma(scale[i])
-          .css()
-          .replace('rgb', 'rgba')
-          .replace(')', ',1.0)')
+        name : `${color.name} ${levels[i]}`,
+        id   : color.name.toLowerCase().replace(/ /g, '-'),
+        hex  : scale[i],
+        rgb  : chroma(scale[i]).css(),
+        rgba : chroma(scale[i]).css().replace('rgb', 'rgba').replace(')', ',1.0)')
       });
     }
   }
@@ -31,19 +39,14 @@ function generatePalette (starterPalette) {
 function getRange (hexColor) {
   const end = '#fff';
   return [
-    chroma(hexColor)
-      .darken(1.4)
-      .hex(),
+    chroma(hexColor).darken(1.4).hex(),
     hexColor,
     end
   ];
 }
 
 function getScale (hexColor, numberOfColors) {
-  return chroma
-    .scale(getRange(hexColor))
-    .mode('lab')
-    .colors(numberOfColors);
+  return chroma.scale(getRange(hexColor)).mode('lab').colors(numberOfColors);
 }
 
 export { generatePalette };
--- src/components/NewPaletteForm.js
+++ src/components/NewPaletteForm.js
@@ -101,6 +101,7 @@ class NewPaletteForm extends Component {
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount () {
     ValidatorForm.addValidationRule('isColorNameUnique', (value) =>
@@ -140,6 +141,16 @@ class NewPaletteForm extends Component {
     this.setState({ newName: evt.target.value });
   }
 
+  handleSubmit () {
+    let newName = 'New Test Palette';
+    const newPalette = {
+      paletteName : newName,
+      id          : newName.toLowerCase().replace(/ /g, '-'),
+      colors      : this.state.colors
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push('/');
+  }
   render () {
     const { classes } = this.props;
     const { open } = this.state;
@@ -149,6 +160,7 @@ class NewPaletteForm extends Component {
         <CssBaseline />
         <AppBar
           position="fixed"
+          color="default"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
           })}
@@ -165,6 +177,9 @@ class NewPaletteForm extends Component {
             <Typography variant="h6" color="inherit" noWrap>
               Persistent drawer
             </Typography>
+            <Button variant="contained" color="primary" onClick={this.handleSubmit}>
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
