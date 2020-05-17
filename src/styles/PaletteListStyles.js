0caf22d0ce3aee72ca5712d8359eac7caf98bbb0
sunlee <sunlee128@gmail.com>
2020-05-17 14:11:59 +1000

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
   root      : {
     backgroundColor : 'blue',
@@ -7,11 +8,17 @@ export default {
     justifyContent  : 'center'
   },
   container : {
-    width         : '50%',
-    display       : 'flex',
-    alignItems    : 'flex-start',
-    flexDirection : 'column',
-    flexWrap      : 'wrap'
+    width              : '50%',
+    display            : 'flex',
+    alignItems         : 'flex-start',
+    flexDirection      : 'column',
+    flexWrap           : 'wrap',
+    [sizes.down('xl')]: {
+      width : '80%'
+    },
+    [sizes.down('xs')]: {
+      width : '75%'
+    }
   },
   nav       : {
     display        : 'flex',
@@ -28,6 +35,13 @@ export default {
     width               : '100%',
     display             : 'grid',
     gridTemplateColumns : 'repeat(3, 30%)',
-    gridGap             : '5%'
+    gridGap             : '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns : 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns : 'repeat(1, 100%)',
+      gridGap             : '1rem'
+    }
   }
 };
