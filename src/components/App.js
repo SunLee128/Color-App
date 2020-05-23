8fa3429269f59b0493de0264936f96a0c91b2e09
sunlee <sunlee128@gmail.com>
2020-05-23 20:33:45 +1000

    clean up route animations

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import seedColors from '../seedColors';
 import NewPaletteForm from './NewPaletteForm';
 import { generatePalette } from '../colorHelper';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import './App.css';
+import Page from './Page';
 
 class App extends Component {
   constructor (props) {
@@ -46,15 +46,15 @@ class App extends Component {
       <Route
         render={({ location }) => (
           <TransitionGroup>
-            <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            <CSSTransition key={location.key} classNames="page" timeout={500}>
               <Switch location={location}>
                 <Route
                   exact
                   path="/palette/new"
                   render={(routeProps) => (
-                    <div className="page">
+                    <Page className="page">
                       <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />
-                    </div>
+                    </Page>
                   )}
                 />
                 <Route
@@ -73,18 +73,18 @@ class App extends Component {
                   exact
                   path="/"
                   render={(routeProps) => (
-                    <div className="page">
+                    <Page className="page">
                       <PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps} />
-                    </div>
+                    </Page>
                   )}
                 />
                 <Route
                   exact
                   path="/palette/:id"
                   render={(routeProps) => (
-                    <div className="page">
+                    <Page className="page">
                       <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
-                    </div>
+                    </Page>
                   )}
                 />
               </Switch>
